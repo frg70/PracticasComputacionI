4b7b1054356bb28c037a5c6192ebaed8953302b9
frg70 <fer.ramirez.gon@gmail.com>
2021-09-17 11:22:59 -0500

    Calculadora

---
+++ Calculadora/Calculadora.cpp
@@ -0,0 +1,23 @@
+/* Author: Fernando Ramírez González
+ * fer.ramirez.gon@gmail.com
+ * September 13, 2021
+ */
+#include <iostream>
+using namespace std;
+int main() {
+    float a = 0, b = 0; double result = 0; string c; //Variable definition
+    cout <<"Entre su primer digito: "; cin >> a; cout << "Entre su primer operador: "; cin >> c; cout << "Entre su segundo digito: "; cin >> b; //Input parameters
+    //Compare the sign conditions
+    if (c == "+"){result = a + b;}
+    if (c == "-"){result = a - b;}
+    if (c == "*"){result = a * b;}
+    if (c == "/"){result = a/b;};
+    if (c == "%"){result = fmod(a,b);}
+    //Print out the elements according to Github example
+    cout <<"Primer numero: " << a << endl;
+    cout << " Operacion: " << c << endl;
+    cout << "Segundo numero: " << b << endl;
+    cout << "Resultado: " << endl;
+    cout << a << " " << c <<" "<< b << " = " << result << endl;
+    return 0;
+}

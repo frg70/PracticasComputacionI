22afae6e1938d3b283cced550ee2f5f879bf76d1
frg70 <fer.ramirez.gon@gmail.com>
2021-09-23 21:04:54 -0500

    Arreglos

---
+++ Arreglos/cmake-build-debug/CMakeFiles/Arreglos.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Arreglos.exe"
+  "Arreglos.exe.manifest"
+  "Arreglos.lib"
+  "Arreglos.pdb"
+  "CMakeFiles/Arreglos.dir/Arreglos.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Arreglos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

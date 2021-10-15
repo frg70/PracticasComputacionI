061c8916b7650e31cd798e4b5dd1da4b2ac4d769
frg70 <fer.ramirez.gon@gmail.com>
2021-10-15 15:04:32 -0500

    - m Punto.cpp

---
+++ Puntos/cmake-build-debug/CMakeFiles/Puntos.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Puntos.dir/Punto.cpp.obj"
+  "CMakeFiles/Puntos.dir/Puntos.cpp.obj"
+  "Puntos.exe"
+  "Puntos.exe.manifest"
+  "Puntos.lib"
+  "Puntos.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Puntos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
